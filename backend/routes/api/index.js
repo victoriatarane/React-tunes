531fde6fa1dcc6790958aef7396e974173b00baa
victoriatarane <victoriatarane@gmail.com>
2021-06-22 15:15:59 -0700

    debugging

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const songsRouter = require('/songs.js');
+const songsRouter = require('./songs.js');
 
 
 router.use('/session', sessionRouter);
