4f4bdea246876327d361f9464ceb1492175331ee
victoriatarane <victoriatarane@gmail.com>
2021-06-23 12:48:00 -0700

    minor css

--- frontend/src/components/Songs/song.js
+++ frontend/src/components/Songs/song.js
@@ -1,6 +1,8 @@
 // import { useState } from 'react';
 // import { useDispatch } from 'react-redux';
 // import { useHistory } from 'react-router-dom';
+import './Songs.css';
+
 
 const Song = ({ song, likes }) => {
     // const dispatch = useDispatch();
